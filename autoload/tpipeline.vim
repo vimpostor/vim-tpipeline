vimpostor/vim-tpipeline

commit 224e79dd7c9e20459f50822eafb908ba8e4e6727

Defer updates of CmdlineEnter
Same reasoning as for ee1e7dfe8f26890c651d18134ca32a16edfc19ca

The problem was that sometimes when a mapping uses a silent : the mode()
will shortly enter Cmdline and we will show "Command" mode in the
statusline.

In the future we should probably improve on this further to prevent
flickering between 'Insert' and 'Command'

autoload/tpipeline.vim
@@ -16,8 +16,8 @@ func tpipeline#build_hooks()
 			autocmd FocusLost * call tpipeline#cautious_cleanup()
 		endif
 		autocmd VimLeavePre * call tpipeline#cleanup()
-		autocmd BufEnter,InsertLeave,CursorHold,CursorHoldI,CmdlineEnter * call tpipeline#update()
-		autocmd InsertEnter * call tpipeline#deferred_update()
+		autocmd BufEnter,InsertLeave,CursorHold,CursorHoldI * call tpipeline#update()
+		autocmd InsertEnter,CmdlineEnter * call tpipeline#deferred_update()
 		if g:tpipeline_cursormoved
 			autocmd CursorMoved * call tpipeline#update()
 		endif

commit 01d4073e7f1319f223c0d5bfd1abe1e292238252

Do not update twice on deferred update
Fixes flickering.
Regression was introduced in 224e79dd7c9e20459f50822eafb908ba8e4e6727

autoload/tpipeline.vim
@@ -116,7 +116,11 @@ endfunc
 
 func tpipeline#deferred_update()
 	let s:update_required = 1
-	call tpipeline#update()
+	if s:update_pending
+		return
+	endif
+	let s:update_pending = 1
+	let s:delay_timer = timer_start(s:update_delay, {-> tpipeline#delayed_update()})
 endfunc
 
 func tpipeline#delayed_update()