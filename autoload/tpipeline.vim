vimpostor/vim-tpipeline

commit c3efbbbc2b9e54fb5e417f0010316616a186acff

Add focus support for g:tpipeline_restore
This is a continuation of b9311975cde0a312a2b3e6c147e1ffe0dd26c5bf

Restoring the tmux statusline is feature-complete now.

Still we should test this a bit more before we enable it by default.

autoload/tpipeline.vim
@@ -236,12 +236,16 @@ func tpipeline#update()
 	endif
 endfunc
 
+func tpipeline#restore_tmux()
+	call system('tmux set -g status-left ' . shellescape(s:restore_left))
+	if g:tpipeline_split
+		call system('tmux set -g status-right ' . shellescape(s:restore_right))
+	endif
+endfunc
+
 func tpipeline#cleanup()
 	if g:tpipeline_restore
-		call system('tmux set -g status-left ' . shellescape(s:restore_left))
-		if g:tpipeline_split
-			call system('tmux set -g status-right ' . shellescape(s:restore_right))
-		endif
+		call tpipeline#restore_tmux()
 	endif
 	if s:is_nvim
 		call jobstop(s:job)
@@ -258,6 +262,12 @@ endfunc
 func tpipeline#forceupdate()
 	let s:last_statusline = ''
 	call tpipeline#update()
+	if g:tpipeline_restore
+		call system("tmux set -g status-left '#(cat #{socket_path}-\\#{session_id}-vimbridge)'")
+		if g:tpipeline_split
+			call system("tmux set -g status-right '#(cat #{socket_path}-\\#{session_id}-vimbridge-R)'")
+		endif
+	endif
 endfunc
 
 func tpipeline#cautious_cleanup()
@@ -278,6 +288,9 @@ func tpipeline#cautious_cleanup()
 			call chansend(s:channel, cstream)
 		else
 			call ch_sendraw(s:channel, cstream)
+		endif
+		if g:tpipeline_restore
+			call tpipeline#restore_tmux()
 		endif
 	endif
 endfunc

commit e1d71a793af6bb5476745f543e98a2a254911a3b

Always update after the socket path has been set
Regression was introduced in c3efbbb
This fixes a race condition where the statusline would appear empty
after switching from a non-vim pane to a vim-pane until the tmux
statusline was manually refreshed.

autoload/tpipeline.vim
@@ -260,14 +260,14 @@ func tpipeline#cleanup()
 endfunc
 
 func tpipeline#forceupdate()
-	let s:last_statusline = ''
-	call tpipeline#update()
 	if g:tpipeline_restore
 		call system("tmux set -g status-left '#(cat #{socket_path}-\\#{session_id}-vimbridge)'")
 		if g:tpipeline_split
 			call system("tmux set -g status-right '#(cat #{socket_path}-\\#{session_id}-vimbridge-R)'")
 		endif
 	endif
+	let s:last_statusline = ''
+	call tpipeline#update()
 endfunc
 
 func tpipeline#cautious_cleanup()