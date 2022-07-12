vimpostor/vim-tpipeline

commit 211c4381f55d50a0dbfff6208069fc4123d476ce

Do not load in GUI vim by default
Obviously if GUI vim is started outside of tmux, we always already
disabled this plugin, since $TMUX would be empty then.
But if someone starts GUI vim from the command line within tmux, then
$TMUX would not be empty and the plugin would become enabled which would
look very weird, because we then embed the GUI vim statusline in a
remote tmux console window.

For gVim fixing this is very easy, as we can simply check for
has('gui_running').

Unfortunately, this variable will probably not work in neovim GUIs,
since the neovim GUI only attaches after vimrc has already been sourced,
so has('gui_running') will return 0 even though the GUI will attach
later in the startup process.

We do not want to use neovim-qt specific variables like g:GuiLoaded [1].
For example gonvim uses another variable [2].

This is where things get more complicated.
It seems like the only reliable way to detect it is to use the UIEnter
autocmd [3], which luckily exists since 2017 [4].

There is also a way to identify neovim clients over RPC [5], but using
autocmds is probably much easier.

Therefore to solve the problem, we disable the plugin on UIEnter, which
we do with the new tpipeline#state#restore() function added in
a95ccb6939c819142f9b972033010403c4b7f5d8.

Theoretically we could also reload the plugin again on UILeave, but in
reality I don't see anyone ever needing to do this.

We could also add an option to keep the plugin enabled for GUIs if
someone wants to have that feature, but let's do that in the future if
that usecase ever comes up, which it probably won't (yes I am jinxing
it).
At least we now have sane defaults for GUI vim.

Fixes #29.

[1] https://github.com/equalsraf/neovim-qt/issues/219
[2] https://github.com/dzhou121/gonvim/issues/17
[3] https://github.com/neovim/neovim/issues/3646
[4] https://github.com/neovim/neovim/pull/6917
[5] https://github.com/neovim/neovim/pull/6743

autoload/tpipeline.vim
@@ -119,6 +119,7 @@ func tpipeline#initialize()
 
 		call tpipeline#util#set_size()
 		au VimResized * call tpipeline#util#set_size()
+		au UIEnter * call tpipeline#state#restore()
 	endif
 endfunc
 

plugin/tpipeline.vim
@@ -1,4 +1,4 @@
-if exists('g:loaded_tpipeline') || empty($TMUX) || !(has('nvim') || has('job'))
+if exists('g:loaded_tpipeline') || empty($TMUX) || !(has('nvim') || has('job')) || has('gui_running')
 	finish
 endif
 let g:loaded_tpipeline = 1

commit c2603e4ad5c2a3011cffc9ea58d2b5036717067e

Fix neovim GUI detection
Regression was introduced in 211c438 and actually disabled this plugin
for neovim entirely, whoopsie and sorry for that!

The neovim GUI detection is a single dumpster fire:

Why do we have to listen to autocmds?
Why can't we just use has('gui_running') at any point, like in vim and
gVim?
Why can't we just use UIEnter and must check an extra flag?

Who in their right mind would design such a convoluted clusterfuck,
especially if vim already has such a sane API for this, BUT NO WE HAVE
TO BE FUCKING SPECIAL IN NEOVIM.
All the explanations are just lazy: Oh, you can't have gui_running set
on startup, because plugins are sourced before the GUI attaches?
Oh ok, let's just make it hard for our users then, so that we don't have
to redesign our shit spaghetti code, what a great idea!?!

The VimEnter autocmd already is there for all TUI startup needs. Why
does UIEnter trigger for TUI startup too???

To fix this mess, we check for the chan flag on UIEnter, which is 1 if
we are in a GUI and 0 if we are in a TUI.

Fixes #30

autoload/tpipeline.vim
@@ -119,7 +119,7 @@ func tpipeline#initialize()
 
 		call tpipeline#util#set_size()
 		au VimResized * call tpipeline#util#set_size()
-		au UIEnter * call tpipeline#state#restore()
+		au UIEnter * if v:event['chan'] | call tpipeline#state#restore() | endif
 	endif
 endfunc
 