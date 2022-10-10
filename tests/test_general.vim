vimpostor/vim-tpipeline

commit c345f17e8ff7c3158a60d7a4285fc29972fa0c80

Evaluate inner commands in old style
The neovim path is not affected by this.

Otherwise if inner is set to something like the following line:
exists('w:quickfix_title')? ' '.w:quickfix_title : ''

We get the following issue:
Fehler beim Ausführen von "function <lambda>5[1]..tpipeline#delayed_update[10]..tpipeline#update[17]..tpipeline#parse#Parse_stl[21]..<SNR>41_Parse":
Zeile   45:
E1004: Leerzeichen vor und nach '?' benötigt bei "? ' '.w:quickfix_title : ''"

Therefore, for the vim9 path we must evaluate this part with legacy
script :(

This partly supersedes 7c820414b7ba6280e774c10df4bea6798ce5b031

Let's see if we also have to evaluate the eval() in Parse_stl() like
this in the future.

autoload/tpipeline/parse.vim
@@ -48,7 +48,8 @@ def Parse(opt: string): string
 		# handle multichar arguments
 		var inner = strcharpart(opt, 1, len - 2)
 		if first ==# '{'
-			return Parse_stl("" .. eval(inner))
+			exe printf('legacy let g:tpipeline_leg = eval("%s")', inner)
+			return Parse_stl("" .. g:tpipeline_leg)
 		elseif first ==# '#'
 			var id = synIDtrans(hlID(inner))
 

commit 58daf4b1927c4c4fa5377f7527a2d13d06a044f7

Escape evaluated strings
Regression was introduced in c345f17e8ff7c3158a60d7a4285fc29972fa0c80

We must escape the quotes, otherwise they will end the string wrongly.

autoload/tpipeline/parse.vim
@@ -48,7 +48,7 @@ def Parse(opt: string): string
 		# handle multichar arguments
 		var inner = strcharpart(opt, 1, len - 2)
 		if first ==# '{'
-			exe printf('legacy let g:tpipeline_leg = eval("%s")', inner)
+			exe printf('legacy let g:tpipeline_leg = eval("%s")', escape(inner, '"'))
 			return Parse_stl("" .. g:tpipeline_leg)
 		elseif first ==# '#'
 			var id = synIDtrans(hlID(inner))

tests/test_general.vim
@@ -185,3 +185,9 @@ func Test_number_evaluation()
 	call Read_socket()
 	call assert_match(string(g:ReturnNumber()), s:left)
 endfunc
+
+func Test_quoted_strings()
+	let g:tpipeline_statusline = '%{eval("g:ReturnNumber()")}'
+	call Read_socket()
+	call assert_match(string(g:ReturnNumber()), s:left)
+endfunc