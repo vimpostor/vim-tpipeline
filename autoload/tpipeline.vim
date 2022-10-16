vimpostor/vim-tpipeline

commit 0ca7d7f55f3c2319b333125d78a0f1a85b22d1c6

Relax assumptions about early stl evaluation
In affe9ca352dabb8eddbcedd0baac4672f9173724 we already postponed the
evaluation for neovim. This was because lualine used some weird
shenanigans to update the statusline that were incompatible with early
evaluation of the statusline.

It looks like lualine is not the only statusline that behaves like that
and it turns out that we can even get the same scenario in vim by using
lightline + vim-lightline-coc.

Thus we need to postpone the evaluation for vim as well.

Fixes #33

autoload/tpipeline.vim
@@ -181,9 +181,6 @@ func tpipeline#init_statusline()
 			if empty(&stl)
 				set stl=%!tpipeline#stl#line()
 			endif
-			if !s:is_nvim
-				let g:tpipeline_statusline = &stl
-			endif
 			if g:tpipeline_clearstl
 				set stl=%#StatusLine#
 			endif
@@ -229,7 +226,11 @@ func tpipeline#update()
 	if s:has_eval_stl
 		let line = luaeval("require'tpipeline.main'.update()")
 	else
-		let line = tpipeline#parse#parse_stl(g:tpipeline_statusline)
+		let stl=g:tpipeline_statusline
+		if empty(stl)
+			let stl = &stl
+		endif
+		let line = tpipeline#parse#parse_stl(stl)
 	endif
 	if line ==# s:last_statusline
 		" don't spam the same message twice

tests/test_general.vim
@@ -152,3 +152,14 @@ func Test_case_insensitive()
 	call Read_socket()
 	call assert_equal('#[fg=#ffffff,bg=#aaaaaa]test', s:left)
 endfunc
+
+func Test_late_evaluation()
+	" stl may change dynamically, even without function indirections
+	let g:tpipeline_statusline = ""
+	for i in range(10)
+		exec 'set stl=' . string(i)
+		call Read_socket()
+		call assert_match(string(i), s:left)
+		call assert_equal('', s:right)
+	endfor
+endfunc

commit ec034b5a867843a5785ad90d7126d71fc29ac2e2

Fix permanent erasure of &stl for g:tpipeline_clearstl
Regression was introduced in 0ca7d7f55f3c2319b333125d78a0f1a85b22d1c6

In this case we need to store the stl in the g:tpipeline_statusline
variable so that we don't lose it, otherwise we will always render an
empty statusline.

See #36

autoload/tpipeline.vim
@@ -184,6 +184,7 @@ func tpipeline#init_statusline()
 				set stl=%!tpipeline#stl#line()
 			endif
 			if g:tpipeline_clearstl
+				let g:tpipeline_statusline = &stl
 				set stl=%#StatusLine#
 			endif
 		endif